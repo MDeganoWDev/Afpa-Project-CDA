403a36bd2be2fe6742f32ced75fef57da32cffbd
PierreSc <98711107+PierreSCHIAVON@users.noreply.github.com>
2023-05-04 09:54:54 +0200

    modif service

--- src/app/services/produit.service.ts
+++ src/app/services/produit.service.ts
@@ -18,4 +18,8 @@ export class ProduitService {
       { observe: 'body' }
     );
   }
+
+  getProduitByName(produit: string): Observable<object> {
+    return  this.http.get(`https://world.openfoodfacts.org/cgi/search.pl?search_terms=${produit}&search_simple=1&action=process&json=1&page_size=20`);
+  }
 }
