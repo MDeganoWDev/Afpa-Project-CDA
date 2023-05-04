38c5beaa1711ef30fb3b4c17900f03cbae665819
Guilherme <bahia.gsa@gmail.com>
2023-05-04 15:55:58 +0200

    Last change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from './Components/footer/footer.component';
 /*import { HeaderComponent } from './header/header.component';*/
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './Components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ProduitService } from './services/produit.service';
-import { FicheProduitComponent } from './components/fiche-produit/fiche-produit.component';
-import { QuickcomponentComponent } from './components/quickcomponent/quickcomponent.component';
+import { FicheProduitComponent } from './Components/fiche-produit/fiche-produit.component';
+import { QuickcomponentComponent } from './Components/quickcomponent/quickcomponent.component';
 
 @NgModule({
   declarations: [
