c972e23346d4ca4173b026869534484a8e4ec3d4
Degano Matteo <mdegano.webdev@gmail.com>
2023-05-04 11:29:16 +0200

    intégration footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';//
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'; //
-import {MatIconModule} from '@angular/material/icon';//
+import { HttpClientModule } from '@angular/common/http'; 
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
